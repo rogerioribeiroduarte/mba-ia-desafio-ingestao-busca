d985a6b001772cee4c0146a1d5a6f4e5afe5cfff
Rogerio Ribeiro Duarte <rogeriorduarte@rduarte.eti.br>
2025-08-29 22:36:33 +0000

    ingest

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Desafio MBA Engenharia de Software com IA - Full Cycle
 
-Descreva abaixo como executar a sua solução.
+Para execução do projeto é preciso seguir os seguintes passos
+1. Ajustar a senha do banco de dados no docker-compose.yml
+2. Subir o banco de dados:
+<pre>
+docker compose up -d
+</pre>
+3. Criar um arquivo .env com as chaves necessárias e senha do banco de dados, conforme o modelo em .env.example (não esquecer de ajustar a senha do banco)
+4. Instalar as dependências
+<pre>
+pip install -r requirements.txt
+</pre>
+5. Executar ingestão do PDF:
+<pre>
+python src/ingest.py
+</pre>
+6. Rodar o chat:
+<pre>
+python src/chat.py
+</pre>
--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,40 @@
+from langchain_postgres import PGVector
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 PDF_PATH = os.getenv("PDF_PATH")
+COLLECTION_NAME=os.getenv("PG_VECTOR_COLLECTION_NAME")
+PGVECTOR_URL=os.getenv("DATABASE_URL")
+os.environ['GOOGLE_API_KEY'] = os.getenv("LLM_API_KEY")
 
 def ingest_pdf():
-    pass
-
+    # Inicializar embedding
+	embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    # Inicializar banco
+	db = PGVector(embeddings=embeddings,collection_name=COLLECTION_NAME,connection=PGVECTOR_URL,use_jsonb=True,)
+    # Carregar arquivo
+	loader = PyPDFLoader(PDF_PATH)
+	docs = loader.load()
+    # Fazer split
+	splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150,)
+	chunks = splitter.split_documents(docs)
+	# Enriquecer splits
+	enriched = [
+	    Document(
+		    page_content=d.page_content,
+            	metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+    	)
+		for d in chunks
+    ]  
+	ids = [f"doc-{i}" for i in range(len(enriched))]
+	# Inserir conteúdo no banco vetorial
+	db.add_documents(documents=enriched, ids=ids)
 
 if __name__ == "__main__":
     ingest_pdf()
